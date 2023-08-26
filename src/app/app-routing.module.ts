cf4b4bfbe91fa86d3fd772603f1808106170efee
IVAN MAULANA FARAJMUHADZIB <if20.ivanfarajmuhadzib@mhs.ubpkarawang.ac.id>
2023-08-26 20:26:16 +0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list user',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list-user',
+    loadChildren: () => import('./pages/list-user/list-user.module').then( m => m.ListUserPageModule)
+  },
+  {
+    path: 'create-user',
+    loadChildren: () => import('./pages/create-user/create-user.module').then( m => m.CreateUserPageModule)
+  },
+  {
+    path: 'update-user',
+    loadChildren: () => import('./pages/update-user/update-user.module').then( m => m.UpdateUserPageModule)
+  },
+  {
+    path: 'delete-user',
+    loadChildren: () => import('./pages/delete-user/delete-user.module').then( m => m.DeleteUserPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'single-user',
+    loadChildren: () => import('./pages/single-user/single-user.module').then( m => m.SingleUserPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
